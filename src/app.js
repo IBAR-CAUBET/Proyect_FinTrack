f8265867b40d55befdf292f2a0d147339e9ca643
IBAR-CAUBET <exequielcaubet@gmail.com>
2025-09-12 22:20:45 -0300

    Proyecto FinTrack: Backend y Frontend

--- src/app.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-const app = require('./server/server')
-require('./db/connection')
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => console.log(`Servidor funcionando en puerto ${PORT}`))
